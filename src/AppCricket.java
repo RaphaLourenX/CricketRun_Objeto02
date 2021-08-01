bf4d22f6daeff760420bcdf0a70ed57556b19e12
RaphaLourenX <83429579+RaphaLourenX@users.noreply.github.com>
2021-08-01 12:00:39 -0300

    Criado o método de pulo dos grilos, junto com os testes iniciais.

--- src/AppCricket.java
+++ src/AppCricket.java
@@ -1,9 +1,27 @@
 
 public class AppCricket {
 	public static int CRICKETNUMBER = 10;
+	public static int GOAL = 200;
 	
 	public static void main(String[] args) {
-	System.out.println("Welcome to the CricketRace! \n Do your bets! \n We have a total of " + CRICKETNUMBER + " crickets running this time!");
+	System.out.println("Welcome to the CricketRace! \n Do your bets! \n We have a total of " + CRICKETNUMBER + " crickets running this time! \n");
+	
+	int add = 0;
+	int winner;
+	
+	Cricket[] crickets = new Cricket[CRICKETNUMBER];
+	
+	for (int i = 0; i < crickets.length; i++) {
+		crickets[i] = new Cricket();
+		crickets[i].id = add;
+		add++;
+		System.out.println("Cricket " + crickets[i].id + ": I'm Ready! \n");
 	}
-
+	
+	//EVERYBODY JUMP!
+	for (int i = 0; i < crickets.length; i++) {
+		crickets[i].doJump();
+	}
+	
+	}
 }
--- src/Cricket.java
+++ src/Cricket.java
@@ -1,17 +1,23 @@
 
 public class Cricket {
-	private int id;
+	// [ATRIBUTOS]
+	public int id = 0;
 	
 	public int jumpNumber; //Total de Pulos que o Grilo deu
-	public int thisJumpDist; //Distância que pulou no pulo atual
-	public int jumoTotalDist; //Distância total percorrida
+	public int jumpDist; //Distância que pulou no pulo atual
+	public int totalDist; //Distância total percorrida
+	
+	public int randomJump() {return (int)(1 + Math.random() * 20);}
+	
 	
 	public void doJump() {
+		this.jumpNumber++;
+		this.jumpDist = randomJump();
+		this.totalDist += this.jumpDist;
+		System.out.println("Cricket " + this.id + " jumped " + this.jumpDist + "cm. \n"
+				+ "Cricket " + this.id + " total distance: " + this.totalDist+ "cm.\n"
+				+ "Cricket " + this.id + " jumped " + this.jumpNumber + " times. \n");
 		
-	}
-	
-	public int randomJump() {
-		return (int)(1 + Math.random() * 20);
 	}
 
 }
