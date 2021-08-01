3f2ac311141073186409dd5b42361873139bf73f
izack <izackcosta97@gmail.com>
2021-08-01 12:17:15 -0300

    Iniciado algorítmo de detecção de vencedor

--- src/AppCricket.java
+++ src/AppCricket.java
@@ -1,13 +1,14 @@
 
 public class AppCricket {
-	public static int CRICKETNUMBER = 10;
+	public static int CRICKETNUMBER = 1000;
 	public static int GOAL = 200;
 	
 	public static void main(String[] args) {
 	System.out.println("Welcome to the CricketRace! \n Do your bets! \n We have a total of " + CRICKETNUMBER + " crickets running this time! \n");
 	
 	int add = 0;
-	int winner;
+	Cricket winner;
+	boolean hasWinner = false;
 	
 	Cricket[] crickets = new Cricket[CRICKETNUMBER];
 	
@@ -19,8 +20,17 @@ public class AppCricket {
 	}
 	
 	//EVERYBODY JUMP!
+	while(true) {
 	for (int i = 0; i < crickets.length; i++) {
 		crickets[i].doJump();
+		if(crickets[i].totalDist > GOAL) 
+		{
+			winner = crickets[i];
+		    System.out.println("The winner is Cricket" + crickets[i].id + ".");
+		    hasWinner = true;
+		}
+	}
+	if(hasWinner) break;
 	}
 	
 	}
